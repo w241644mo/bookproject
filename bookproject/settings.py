26b926b0bac22b750f57467d134c09fcf482626b
tail <w241644mo@gmail.com>
2025-06-21 19:45:26 +0900

    second6

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -141,9 +141,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# collectstaticのために常に必要
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
